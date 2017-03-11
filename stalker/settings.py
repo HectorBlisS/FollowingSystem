80390ac6e60887302f6ffae8f0b637a2eebd09d0
BlisS <contacto@fixter.org>
2017-03-11 14:16:15 -0600

    pedrs2

--- stalker/settings.py
+++ stalker/settings.py
@@ -3,8 +3,8 @@ import os
 MYKEY = os.environ['MYKEY']
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
